e7fd7e76ad4c9537108984eb067d8ddf3e1d442a
Maciej Gielert <glrtdev@gmail.com>
2026-08-16 20:35:50 +0200

    Clean up dead code

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,5 +1,4 @@
 import HeroSection from './components/HeroSection';
-import GameSection from './components/GameSection';
 import ThemeToggle from './components/ThemeToggle';
 import ProjectCard from './components/ProjectCard';
 import React from 'react';
